99676fc1a4ea58c83f931cc01ed2df5deaa06a3b
Harsh2390 <hs4465@nyu.edu>
2024-11-14 18:32:01 -0500

    update for youtube api this update include all the influencers requried right now and also have some efficiency improvements such as error handeling.

--- scraping/youtube_scraper.py
+++ scraping/youtube_scraper.py
@@ -2,6 +2,7 @@
 import creds
 from googleapiclient.discovery import build
 import pandas as pd
+from googleapiclient.errors import HttpError
 #import seaborn as sns
 
 # set up the YouTube API
@@ -11,10 +12,26 @@ import pandas as pd
 
 API_KEY = creds.YT_api
 # channel_ids is a list of channel ids for the influencers
-channel_ids = ['UCX6OQ3DkcsbYNE6H8uQQuVA',
-                'UCxgAuX3XZROujMmGphN_scA',
-                'UCqECaJ8Gagnn7YCbPEzWH6g',
-                'UCIwFjwMjI0y7PDBVEO9-bkQ']
+channel_ids = ['UCqECaJ8Gagnn7YCbPEzWH6g',
+                'UCs6eXM7s8Vl5WcECcRHc2qQ',
+                'UCX6OQ3DkcsbYNE6H8uQQuVA',
+                'UC0WP5P-ufpRfjbNrmOWwLBQ',
+                'UCIwFjwMjI0y7PDBVEO9-bkQ',
+                'UCcgVECVN4OKV6DH1jLkqmcA',
+                'UCxMAbVFmxKUVGAll0WVGpFw',
+                'UCByOQJjav0CUDwxCk-jVNRQ',
+                'UCPlVQrTbx0UxYtilJ3CVu0Q',
+                'UCcgqSM4YEo5vVQpqwN-MaNw',
+                'UCiGm_E4ZwYSHV3bcW1pnSeQ',
+                'UCKuHFYu3smtrl2AwwMOXOlg',
+                'UCBdw4dLCLLHmTgAOnW4V0hQ',
+                'UCzQUP1qoWDoEbmsQxvdjxgQ',
+                'UCcYrdFJF7hmPXRNaWdrko4w',
+                'UCNL1ZadSjHpjm4q9j2sVtOA',
+                'UCvCfpQXRXdJdL07pzTIA6Cw',
+                'UC9CoOnJkIBMdeijd9qYoT_g',
+                'UCPNxhDvTcytIdvwXWAm43cA',
+                'UCtinbF-Q-fVthA0qrFQTgXQ']
 
 # setting up the YouTube API service by defining the API service name and version
 api_service_name = "youtube"
@@ -55,20 +72,44 @@ def get_latest_video_link(youtube, playlist_id): # using the playlist id that we
     return video_title, video_link
 
 # function to get the top comments from the latest video
-def get_top_comments(youtube, video_id): # using the video id that we fetched earlier using the get_latest_video_link function
-    request = youtube.commentThreads().list(
-        part="snippet",
-        videoId=video_id,
-        maxResults=10, # we can tweak this number to get more or less comments
-        order="relevance" # getting the top comments based on relevance
-    )
-    response = request.execute()
-    comments_data = []
-    # loop through the response and store the top comments in the comments_data list
-    for item in response['items']:
-        comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-        comments_data.append(comment) # append the comment only to the comments_data list
-    return comments_data
+# def get_top_comments(youtube, video_id): # using the video id that we fetched earlier using the get_latest_video_link function
+#     request = youtube.commentThreads().list(
+#         part="snippet",
+#         videoId=video_id,
+#         maxResults=10, # we can tweak this number to get more or less comments
+#         order="relevance" # getting the top comments based on relevance
+#     )
+#     response = request.execute()
+#     comments_data = []
+#     # loop through the response and store the top comments in the comments_data list
+#     for item in response['items']:
+#         comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+#         comments_data.append(comment) # append the comment only to the comments_data list
+#     return comments_data
+
+
+
+def get_top_comments(youtube, video_id):
+    try:
+        request = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            maxResults=10,
+            order="relevance"
+        )
+        response = request.execute()
+        comments_data = []
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments_data.append(comment)
+        return comments_data
+    except HttpError as e:
+        # Handle the specific case where comments are disabled
+        if e.resp.status == 403:
+            print(f"Comments disabled for video ID: {video_id}")
+            return []  # Return an empty list if comments are disabled
+        else:
+            raise  # Re-raise unexpected errors to be handled elsewhere
 
 #main code to fetch data and merge into a single DataFrame
 channel_data = get_channel_stats(youtube, channel_ids)
